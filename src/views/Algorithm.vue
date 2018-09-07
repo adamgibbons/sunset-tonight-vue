4f0147a9d5fd11f52a08d77c9aec6cd6267dd580
Adam Gibbons <adam.d.gibbons@gmail.com>
2018-09-06 20:28:37 -0600

    scaffold algorithm interactivity

---
+++ src/views/Algorithm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="range" v-model="variables.cloudTypes">
+    <hr>
+    <pre>
+      {{variables}}
+    </pre>
+    <hr>
+    <p>sunset grade:</p>
+    <pre>{{sunsetGrade}}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      variables: {
+        cloudTypes: null,
+        cloudCoverage: null,
+        airQuality: null,
+        humidity: null,
+        windspeed: null,
+        visibility: null
+      }
+    }
+  },
+  computed: {
+    sunsetGrade () {
+      if (this.variables.cloudTypes) {
+        return parseInt(this.variables.cloudTypes) > 50
+      }
+
+      return false
+    }
+  }
+}
+</script>
