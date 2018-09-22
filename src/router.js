42103a88843378248e0b9ea8caf2a21311cd3791
Adam Gibbons <adam.d.gibbons@gmail.com>
2018-09-22 00:55:03 -0400

    move grade sunset to debug route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Algorithm from './views/Algorithm.vue'
+import Debug from './views/Debug.vue'
 
 Vue.use(Router)
 
@@ -17,10 +18,12 @@ export default new Router({
     {
       path: '/algorithm',
       name: 'algorithm',
-      // route level code-splitting
-      // this generates a separate chunk (Algorithm.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: Algorithm
+    },
+    {
+      path: '/debug',
+      name: 'debug',
+      component: Debug
     }
   ]
 })
--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="home">
-    <!-- <img alt="DebugTonight logo" src="../assets/logo.png"> -->
-    <!-- <Debug /> -->
-    <button @click="init()">
-      Check tonight's sunset
-    </button>
-    <div>
-      {{sunsetGrade}}  
-    </div>
+    <div>sunset grade: {{sunsetGrade}}</div>
+    <hr>
+    <button @click="init">Get my sunset</button>
+    <p>
+      1. Determine your coordinates:
+      <pre>{{coordinates}}</pre>
+    </p>
+    <p>
+      2. Determine time of tonight's sunset:
+      <pre>{{sunset || '-'}}</pre>
+    </p>
+    <p>
+      3. Get forecast at sunset
+      <pre>{{microForecast || '-'}}</pre>
+    </p>
+
+    <p>Powered by Dark Sky</p>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
-import Grade from '@/components/Grade.vue'
 import gradeSunset from '@/utils/grade-sunset'
 
 function handleError ({ code }) {
@@ -21,10 +29,7 @@ function handleError ({ code }) {
 }
 
 export default {
-  name: 'home',
-  components: {
-    Grade
-  },
+  name: 'Sunset',
   computed: {
     ...mapGetters(['coordinates', 'sunset', 'forecast']),
     microForecast () {
@@ -59,3 +64,27 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+p, pre {
+  text-align: left;
+}
+
+pre {
+  margin-left: 5em;
+}
+</style>
